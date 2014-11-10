289d6207a944c245916a3b33c29ac9042aab9c7e
Ashish Shrestha <axhixh@users.noreply.github.com>
2014-11-10 10:44:53 +0000

    experiment 4 using queues of length 1

---
+++ exp4/receive-one-slow.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+    "time"
+)
+
+func handleError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func main() {
+	fmt.Println("Receiving message")
+	conn, err := amqp.Dial("amqp://admin:VgkDl7PM5DzY@172.17.0.3:5672/")
+	handleError(err, "Unable to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	handleError(err, "Unable to open a channel")
+	defer ch.Close()
+
+    err = ch.ExchangeDeclare("chash-one", "x-consistent-hash", true, false, false, false, nil)
+    handleError(err, "Unable to declare exchange")
+
+    prop := amqp.Table{"x-max-length": int64(1)}
+	q, err := ch.QueueDeclare("", true, false, true, false, prop)
+	handleError(err, "Unable to create queue")
+
+    err = ch.QueueBind(q.Name, "100", "chash-one", false, nil)
+	msg, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	handleError(err, "Failed to register consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msg {
+			log.Printf("%s: %s", q.Name, d.Body)
+            time.Sleep(3 * time.Second)
+		}
+	}()
+
+	log.Printf("[*] Waiting. Press CTRL+C to exit")
+	<-forever
+}
