ad924a9e405133d3f6afbebbf38485b1eb2090fb
Ashish Shrestha <axhixh@users.noreply.github.com>
2014-11-22 10:57:32 +0000

    refactored to use common code

--- exp4/receive-one.go
+++ exp4/receive-one.go
@@ -1,38 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/axhixh/rabbitmq-experiments/common"
 	"github.com/streadway/amqp"
 	"log"
 )
 
-func handleError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
-	}
-}
-
 func main() {
-	fmt.Println("Receiving message")
+	log.Println("Receiving message")
 	conn, err := amqp.Dial("amqp://admin:VgkDl7PM5DzY@172.17.0.3:5672/")
-	handleError(err, "Unable to connect to RabbitMQ")
+	common.HandleError(err, "Unable to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	handleError(err, "Unable to open a channel")
+	common.HandleError(err, "Unable to open a channel")
 	defer ch.Close()
 
-    err = ch.ExchangeDeclare("chash-one", "x-consistent-hash", true, false, false, false, nil)
-    handleError(err, "Unable to declare exchange")
+	err = ch.ExchangeDeclare("chash-one", "x-consistent-hash", true, false, false, false, nil)
+	common.HandleError(err, "Unable to declare exchange")
 
-    props := amqp.Table{"x-max-length": int64(1)}
+	props := amqp.Table{"x-max-length": int64(1)}
 	q, err := ch.QueueDeclare("", true, false, true, false, props)
-	handleError(err, "Unable to create queue")
+	common.HandleError(err, "Unable to create queue")
 
-    err = ch.QueueBind(q.Name, "100", "chash-one", false, nil)
+	err = ch.QueueBind(q.Name, "100", "chash-one", false, nil)
 	msg, err := ch.Consume(q.Name, "", true, false, false, false, nil)
-	handleError(err, "Failed to register consumer")
+	common.HandleError(err, "Failed to register consumer")
 
 	forever := make(chan bool)
 
--- exp4/send-one.go
+++ exp4/send-one.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/axhixh/rabbitmq-experiments/common"
 	"github.com/streadway/amqp"
 	"log"
-    "math/rand"
-    "time"
+	"math/rand"
+	"time"
 )
-
-func handleError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
-	}
-}
 
 func producer(ch chan string, name string, max int) {
 	for i := 0; i < max; i++ {
@@ -24,38 +18,39 @@ func producer(ch chan string, name string, max int) {
 }
 
 func main() {
-	fmt.Println("Sending message")
+	log.Println("Sending message")
 	conn, err := amqp.Dial("amqp://admin:VgkDl7PM5DzY@172.17.0.3:5672/")
-	handleError(err, "Unable to connect to RabbitMQ")
+	common.HandleError(err, "Unable to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	handleError(err, "Unable to open a channel")
+	common.HandleError(err, "Unable to open a channel")
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare("chash-one", "x-consistent-hash", true, false, false, false, nil)
-	handleError(err, "Unable to declare exchange")
+	common.HandleError(err, "Unable to declare exchange")
 
-	msgCh := make(chan string)
+	msgCh := make(chan common.Message)
 
-    r := rand.New(rand.NewSource(time.Now().Unix()))
+	r := rand.New(rand.NewSource(time.Now().Unix()))
 	categories := []string{"AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH"}
 	for i, cat := range categories {
-        m := fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", 40 + i, cat)
-		go producer(msgCh, m, 50)
-		time.Sleep(time.Duration(r.Intn(200) + r.Intn(200)) * time.Millisecond)
+		gen := common.Generator{Key: cat, Color: 41 + i}
+		log.Printf("Starting %s", gen.Key)
+		go gen.Generate(msgCh, 20)
+		time.Sleep(time.Duration(r.Intn(200)+r.Intn(200)) * time.Millisecond)
 	}
 
 	counter := len(categories)
 
 	for msg := <-msgCh; ; msg = <-msgCh {
-		if "done" == msg {
+		if "done" == msg.Body {
 			counter = counter - 1
 		} else {
-			err = ch.Publish("chash-one", msg[7:9], false, false, amqp.Publishing{
+			err = ch.Publish("chash-one", msg.Key, false, false, amqp.Publishing{
 				ContentType: "text/plain",
-				Body:        []byte(msg)})
-			handleError(err, "unable to send message")
+				Body:        []byte(msg.Body)})
+			common.HandleError(err, "unable to send message")
 
 		}
 		if counter == 0 {
